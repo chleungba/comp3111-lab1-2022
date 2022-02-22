c51f23bb03cb64fcb5d9c268aadbc36aa5708f62
Hayden <hayden.intern@gremod.com>
2022-02-22 16:40:10 +0800

    new changes

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -2,14 +2,15 @@ package Lab1;
 
 public class Library {
 	
+	public boolean someLibraryMethod() {
+        return true;
+    }
+	
 	/* Add this function */
 	public static void main(String arg[]) {
 		Library lib = new Library();
 		if (lib.someLibraryMethod())
 			System.out.println("Hello, World!");
 	}
-		
-	public boolean someLibraryMethod() {
-        return true;
-    }
+
 }
